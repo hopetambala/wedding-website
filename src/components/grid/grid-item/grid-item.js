61bd6eeb5db7250352b30acdb37cf756d0a09f3f
Hope Tambala <hopetambala@outlook.com>
2022-12-01 10:33:48 -0500

    chore: remove margin

--- src/components/grid/grid-item/grid-item.js
+++ src/components/grid/grid-item/grid-item.js
@@ -1,6 +1,5 @@
 import React from "react";
-import * as styles from "./grid-item.module.css";
 
 export const GridItem = ({ children }) => {
-  return <div className={styles.item}>{children}</div>;
+  return <div>{children}</div>;
 };
